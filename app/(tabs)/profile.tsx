a44f0686a18a2d5b9717ef470310b094a150d249
DeLucca990 <pedruca2001@gmail.com>
2023-12-19 12:22:20 -0300

    Authetication Flow

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, Button } from "react-native";
+import React from "react";
+import { useAuth } from "@clerk/clerk-react";
+import { Link } from "expo-router";
 
 const Page = () => {
-    return (
-        <View>
-            <Text>Profile</Text>
-        </View>
-    );
-}
+  const { signOut, isSignedIn } = useAuth();
+
+  return (
+    <View>
+      <Button title="Log Out" onPress={() => signOut()} />
+      {!isSignedIn && (
+        <Link href={"/(modals)/login"}>
+          <Text>Log In</Text>
+        </Link>
+      )}
+    </View>
+  );
+};
 
 export default Page;
